285e2b950e6517aa3e20fd070a2be2dce8e58d34
GabyFutemma <gaby.futemma@gmail.com>
2018-09-05 19:33:53 -0300

    changing Regex and package name

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Example:
 
 ```
 $node
-> let extractLib = require("gftm-extractlinks")
+> let extractLib = require("gf-extract-links-lib")
 > extractLib.getLinksFromMd( "Oi você quer entrar no site [google](www.google.com) ?"); //[{href: "www.google.com", text: "google"}]
 ```
 
@@ -23,15 +23,12 @@ $node
 ## Installation
 - You must have node + npm installed.
 - Installation Guide, visit the [official site](https://www.npmjs.com/get-npm).
-- Proceed installation `$npm install gftm-extractlinks`
+- Proceed installation `$npm install gf-extract-links-lib`
 
 
 ## Roadmap of project
 **version 2.0.0**
 - README in pt_br;
 
-**version 1.0.1 (released)**
-- Using eslint in files;
-
-**version 1.0.0**
-- Functionalities:  extract links and text from markdown;
+**version 1.0.0 (released)**
+- Functionalities: extract links and text from markdown;
--- index.js
+++ index.js
@@ -5,18 +5,19 @@ module.exports.getLinksFromMd = function getLinksFromMd(text) {
 
   if (typeof text === "number" || typeof text !== "string") {
     throw new TypeError("Please insert a text!");
-  } else {
-    var result = [];
-    let regex_url = new RegExp (/(https?:\/\/)?(www\.)?[a-z0-9]*(\.\w{2,})(\.[a-z0-9]*)?(\/.[a-z0-9]*)?(\/.[a-z0-9]*)?/gi);
-    let regex_markdown = new RegExp (/(?<=\[)([a-z0-9]*)(?=\])/gi);
-    let urls = text.match(regex_url);
-    let txts_md = text.match(regex_markdown);
-    if (urls !== null && txts_md !== null) {
-      result = urls.map((url, i) => ({
-        "href": url,
-        "text": txts_md[i]
-      }));
-    }
-    return urls ? result : [];
   }
+
+  var result = [];
+  let regex_url = new RegExp (/(https?:\/\/)?(www\.)?(\w+)(\.*\w*)*(\/*\w*)*(?=\))/gi);
+  let regex_markdown = new RegExp (/(?<=\[)(.*?)(?=\])/gi);
+  let urls = text.match(regex_url);
+  let txts_md = text.match(regex_markdown);
+  if (urls !== null && txts_md !== null) {
+    result = urls.map((url, i) => ({
+      "href": url,
+      "text": txts_md[i]
+    }));
+  }
+  return urls ? result : [];
+
 };
